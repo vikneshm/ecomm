e754f51411841ab316ea60253cf9ec82f6522fad
vikneshm <58290581+vikneshm@users.noreply.github.com>
2020-07-15 17:23:03 +0530

    Updates

---
+++ precache-manifest.860161ad3b5d016c6abe4009ca226789.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d8b9e8fa1b0d8e94354d48f9283a34",
+    "url": "/ecomm/index.html"
+  },
+  {
+    "revision": "c969e442c06c8d09a76e",
+    "url": "/ecomm/static/css/main.84fbc08c.chunk.css"
+  },
+  {
+    "revision": "d348134bf95ea7abe4ad",
+    "url": "/ecomm/static/js/2.e5fcf832.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ecomm/static/js/2.e5fcf832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c969e442c06c8d09a76e",
+    "url": "/ecomm/static/js/main.b4e53422.chunk.js"
+  },
+  {
+    "revision": "8f17f33bcdb904812850",
+    "url": "/ecomm/static/js/runtime-main.20ef0e0d.js"
+  },
+  {
+    "revision": "1c39a110319d80bafc8be3c923b04838",
+    "url": "/ecomm/static/media/logo.1c39a110.jfif"
+  },
+  {
+    "revision": "9d4e40cef0677037a7acf9a419f6d705",
+    "url": "/ecomm/static/media/search.9d4e40ce.png"
+  }
+]);
